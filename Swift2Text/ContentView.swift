3831812f56ddc542ed050b67619089a61cef6daa
Kyle Peterson <kdpeters@gmail.com>
2024-07-23 23:37:48 -0400

    Initial Commit

---
+++ Swift2Text/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift2Text
+//
+//  Created by Kyle Peterson on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift2Text/Swift2TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift2TextApp.swift
+//  Swift2Text
+//
+//  Created by Kyle Peterson on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift2TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
